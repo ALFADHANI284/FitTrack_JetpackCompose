c69feb000218c8a60e04f25e76b9f1bec702c2bd
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2026-04-28 22:19:45 +0700

    Fix(color): perbaiki code warna

--- app/src/main/java/com/alfa/fittrack/ui/theme/Color.kt
+++ app/src/main/java/com/alfa/fittrack/ui/theme/Color.kt
@@ -6,7 +6,7 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val PrimaryColor = Color(0xFFB200)
+val PrimaryColor = Color(0xFFFFB200)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
