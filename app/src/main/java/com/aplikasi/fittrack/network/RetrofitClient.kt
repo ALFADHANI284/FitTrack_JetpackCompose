4cac9a1bff1654fc115c29d1d7c91a5175e8a608
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2026-05-01 22:14:01 +0700

    Feat(RetrofitClient):tambah code untuk memunculkan error menjadi json

--- app/src/main/java/com/aplikasi/fittrack/network/RetrofitClient.kt
+++ app/src/main/java/com/aplikasi/fittrack/network/RetrofitClient.kt
@@ -1,11 +1,21 @@
 package com.aplikasi.fittrack.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 object RetrofitClient {
     private const val BASE_URL = "http://192.168.1.12:8000/api/"
+
+    val client = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("Accept", "application/json") // Ini kunci biar gak dapet HTML
+                .build()
+            chain.proceed(request)
+        }
+        .build()
 
     val instance: ApiService by lazy {
         Retrofit.Builder()
