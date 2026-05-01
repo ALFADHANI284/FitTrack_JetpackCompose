7c0e69a52fe2e0b8eef16f742da5021fbb4cbfd3
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2026-05-01 22:10:22 +0700

    Feat(ApiService):tambah endpoint login dan register

--- app/src/main/java/com/aplikasi/fittrack/network/ApiService.kt
+++ app/src/main/java/com/aplikasi/fittrack/network/ApiService.kt
@@ -1,11 +1,24 @@
 package com.aplikasi.fittrack.network
 
+import com.aplikasi.fittrack.model.AuthResponse
 import com.aplikasi.fittrack.model.BaseResponse
+import com.aplikasi.fittrack.model.LoginRequest
+import com.aplikasi.fittrack.model.RegisterRequest
 import com.aplikasi.fittrack.model.WorkoutResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
     // Endpoint ini nyambung ke local laravel : http://10.0.2.2:8000/api/workout
     @GET("workouts")
     suspend fun getWorkouts(): BaseResponse
+
+    // Endpoint untuk Login
+    @POST("login")
+    suspend fun loginUser(@Body loginRequest: LoginRequest): AuthResponse
+
+    // Endpoint untuk Register
+    @POST("register")
+    suspend fun registerUser(@Body registerRequest: RegisterRequest): AuthResponse
 }
