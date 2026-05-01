9844dc9351f1543281f31680274cdeac408caa81
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2026-05-01 14:07:00 +0700

    Feat(onboarddingscreen): gtambah import R

--- app/src/main/java/com/aplikasi/fittrack/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/aplikasi/fittrack/ui/onboarding/OnboardingScreen.kt
@@ -18,6 +18,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aplikasi.fittrack.ui.theme.PrimaryColor
+import com.aplikasi.fittrack.R
+
 
 @Composable
 fun OnboardingScreen(
