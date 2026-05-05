75f428d7cc01093defe98ef134d4ab4df3b7cf32
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2026-05-05 21:23:48 +0700

    Feat(Login): tambah navigate (ke halaman admin jika memiliki role admin)

--- app/src/main/java/com/aplikasi/fittrack/ui/screens/auth/Login.kt
+++ app/src/main/java/com/aplikasi/fittrack/ui/screens/auth/Login.kt
@@ -4,11 +4,33 @@ import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,7 +51,8 @@ import kotlinx.coroutines.launch
 @Composable
 fun LoginScreen(
     onNavigateToHome: () -> Unit,
-    onNavigateToRegister: () -> Unit // Buat tombol Sign Up di bawah
+    onNavigateToRegister: () -> Unit, // Buat tombol Sign Up di bawah
+    onNavigateToAdmin: () -> Unit //
 ) {
     // 1. STATE: Untuk API
     var email by remember { mutableStateOf("") }
@@ -137,13 +160,14 @@ fun LoginScreen(
                                     val request = LoginRequest(email, password)
                                     val response = RetrofitClient.instance.loginUser(request)
 
-                                    // (Nanti response.token kita simpan di sini pakai SharedPreferences)
-
-                                    // Munculin Toast
                                     Toast.makeText(context, "Login Berhasil!", Toast.LENGTH_SHORT).show()
 
-                                    // Pindah ke halaman Home/Dashboard
-                                    onNavigateToHome()
+                                    // 2. LOGIKA CABANG BERDASARKAN ROLE
+                                    if (response.role == "admin") {
+                                        onNavigateToAdmin() // Lempar ke Dashboard Admin
+                                    } else {
+                                        onNavigateToHome() // Lempar ke Home biasa
+                                    }
 
                                 } catch (e: retrofit2.HttpException) {
                                     loginResult = "Gagal: Email/Password salah atau server mati"
@@ -210,6 +234,7 @@ fun LoginScreen(
 fun LoginScreenPreview() {
     LoginScreen(
         onNavigateToHome = {},
-        onNavigateToRegister = {}
+        onNavigateToRegister = {},
+        onNavigateToAdmin = {}
     )
 }
