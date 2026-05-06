c296c23782c608a6e89a43fa1829daeac481815a
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2026-05-06 21:49:00 +0700

    Feat(ApiService): tambah endpoint get /workouts (agar bisa di akses oleh admin n user)

--- app/src/main/java/com/aplikasi/fittrack/network/ApiService.kt
+++ app/src/main/java/com/aplikasi/fittrack/network/ApiService.kt
@@ -1,11 +1,11 @@
 package com.aplikasi.fittrack.network
 
 import com.aplikasi.fittrack.model.AuthResponse
-import com.aplikasi.fittrack.model.BaseResponse
 import com.aplikasi.fittrack.model.DefaultResponse
 import com.aplikasi.fittrack.model.LoginRequest
 import com.aplikasi.fittrack.model.RegisterRequest
 import com.aplikasi.fittrack.model.WorkoutRequest
+import com.aplikasi.fittrack.model.WorkoutResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -13,8 +13,12 @@ import retrofit2.http.POST
 
 interface ApiService {
     // Endpoint ini nyambung ke local laravel : http://10.0.2.2:8000/api/workout
+
+    // Ambil Daftar Workout (Admin & User)
     @GET("workouts")
-    suspend fun getWorkouts(): BaseResponse
+    suspend fun getWorkouts(
+        @Header("Authorization") token: String
+    ): List<WorkoutResponse>
 
     // Endpoint untuk Login
     @POST("login")
@@ -23,6 +27,7 @@ interface ApiService {
     // Endpoint untuk Register
     @POST("register")
     suspend fun registerUser(@Body registerRequest: RegisterRequest): AuthResponse
+
     // Tambah Data Workout (Hanya Admin)
     @POST("workouts")
     suspend fun createWorkout(
