75cb3716cb16a730dc605c481732d78f8f20305d
Alfa <alfadhanifahrezan@gmail.com>
2026-05-25 19:47:45 +0700

    pidah folder aja

--- app/src/main/java/com/aplikasi/fittrack/ui/screens/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/aplikasi/fittrack/ui/screens/onboarding/OnboardingScreen.kt
@@ -1,4 +1,4 @@
-package com.aplikasi.fittrack.ui.onboarding
+package com.aplikasi.fittrack.ui.screens.onboarding
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
