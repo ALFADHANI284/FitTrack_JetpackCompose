ec21317da14a03ef1f95ae1a382a82690a1bec00
Alfa <alfadhanifahrezan@gmail.com>
2026-05-26 11:54:34 +0700

    Feat(UserData): tambah deklarasi variabel sesuai dengan BE

--- app/src/main/java/com/aplikasi/fittrack/model/UserData.kt
+++ app/src/main/java/com/aplikasi/fittrack/model/UserData.kt
@@ -1,7 +1,18 @@
 package com.aplikasi.fittrack.model
 
+import com.google.gson.annotations.SerializedName
+
 data class UserData(
-    val id: Int,
+    val id: Int, // Biasanya Laravel otomatis menyertakan ID
     val name: String,
-    val email: String
+    val email: String,
+    val role: String?,
+    @SerializedName("avatar_path") val avatarPath: String?,
+    val points: Int,
+    val tier: String?,
+    @SerializedName("referral_code") val referralCode: String?,
+    @SerializedName("daily_calories_target") val dailyCaloriesTarget: Int?,
+    @SerializedName("daily_protein_target") val dailyProteinTarget: Int?,
+    @SerializedName("daily_carbs_target") val dailyCarbsTarget: Int?,
+    @SerializedName("daily_fat_target") val dailyFatTarget: Int?
 )
