08d9230369b82f4abcda438977c28cbb6f321d9e
Alfa <alfadhanifahrezan@gmail.com>
2026-05-28 22:28:26 +0700

    Feat(WorkoutRequest): tambah val

--- app/src/main/java/com/aplikasi/fittrack/model/WorkoutRequest.kt
+++ app/src/main/java/com/aplikasi/fittrack/model/WorkoutRequest.kt
@@ -3,7 +3,8 @@ package com.aplikasi.fittrack.model
 data class WorkoutRequest(
     val category_id: Int,
     val name: String,
-    val duration_minutes: Int,
-    val calories_burned: Int,
-    val description: String
+    val duration_minutes: Int? =null,
+    val calories_burned: Int? =null,
+    val description: String? =null,
+    val link_yt: String? = null
 )
