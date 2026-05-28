40d238dea243a6df010cbc1147bce36c7f8c094e
Alfa <alfadhanifahrezan@gmail.com>
2026-05-28 22:28:42 +0700

    Feat(WorkoutResponse): tambah val agar sesuai

--- app/src/main/java/com/aplikasi/fittrack/model/WorkoutResponse.kt
+++ app/src/main/java/com/aplikasi/fittrack/model/WorkoutResponse.kt
@@ -4,7 +4,8 @@ data class WorkoutResponse(
     val id: Int,
     val category_id: Int,
     val name: String,
-    val duration_minutes: Int,
-    val calories_burned: Int,
-    val description: String
+    val duration_minutes: Int? =null,
+    val calories_burned: Int? =null,
+    val description: String? =null,
+    val link_yt: String? = null
 )
