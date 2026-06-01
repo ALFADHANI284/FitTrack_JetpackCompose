1068c55eca3d43f098259a9209d48f5fa1ee8db1
Alfa <alfadhanifahrezan@gmail.com>
2026-06-01 14:05:22 +0700

    Feat(Gradle): tambah import library tersebut

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 }
