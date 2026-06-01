856119ece9dae83e0c6a26e78bd928e5cd5083fc
Alfa <alfadhanifahrezan@gmail.com>
2026-06-01 22:34:44 +0700

    Feat(WorkoutDetail): tambah val is favorite

--- app/src/main/java/com/aplikasi/fittrack/model/WorkoutResponse.kt
+++ app/src/main/java/com/aplikasi/fittrack/model/WorkoutResponse.kt
@@ -1,5 +1,7 @@
 package com.aplikasi.fittrack.model
 
+import com.google.gson.annotations.SerializedName
+
 data class WorkoutResponse(
     val id: Int,
     val category_id: Int,
@@ -7,5 +9,7 @@ data class WorkoutResponse(
     val duration_minutes: Int? =null,
     val calories_burned: Int? =null,
     val description: String? =null,
-    val link_yt: String? = null
+    val link_yt: String? = null,
+    @SerializedName("is_favorite")
+    val isFavorite: Boolean? = false
 )
