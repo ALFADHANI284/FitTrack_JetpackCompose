c1892bd95ebe7ecb0f0474285e4d2d4f391e6839
Alfa <alfadhanifahrezan@gmail.com>
2026-06-03 13:16:51 +0700

    Feat(Achivement): tambah val

--- app/src/main/java/com/aplikasi/fittrack/model/Achievement.kt
+++ app/src/main/java/com/aplikasi/fittrack/model/Achievement.kt
@@ -1,8 +1,13 @@
 package com.aplikasi.fittrack.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Achievement(
+    val id: Int,
     val name: String,
     val description: String,
-    val badge_icon: String,
-    val points: Int
+    @SerializedName("badge_icon") val badgeIcon: String,
+    val points: Int,
+    @SerializedName("is_claimed") val isClaimed: Boolean = false,
+    @SerializedName("is_unlocked") val isUnlocked: Boolean = false
 )
